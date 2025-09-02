df00e97d24187da9481ce54bd1308b3c34d01b4b
AlayaStHill <alaya.st.hill@gmail.com>
2025-09-02 15:08:23 +0200

    Add project files.

---
+++ Assignment1/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment1.Models;
+
+internal class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Price { get; set; }
+}
