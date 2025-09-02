629b199c669fb263a0ccf7c8b2fc702165577aeb
AlayaStHill <alaya.st.hill@gmail.com>
2025-09-02 15:18:56 +0200

    Correct startingpoint

--- Assignment1/Services/MenuService.cs
+++ Assignment1/Services/MenuService.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Assignment1.Services;
 
-namespace Assignment1.Services
+internal class MenuService
 {
-    internal class MenuService
-    {
-        
-    }
+    
 }
--- Assignment1/Services/ProductService.cs
+++ Assignment1/Services/ProductService.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Assignment1.Services;
 
-namespace Assignment1.Services
+internal class ProductService
 {
-    internal class ProductService
-    {
-    }
 }
