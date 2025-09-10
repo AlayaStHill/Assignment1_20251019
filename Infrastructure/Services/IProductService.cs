499b847e3212c3b9573a662a7b9fc54c47f6368e
AlayaStHill <alaya.st.hill@gmail.com>
2025-09-10 12:45:51 +0200

    Interfaces except JsonFileService, completed

---
+++ Infrastructure/Services/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Services
+{
+    public interface IProductService
+    {
+        void CreateProduct(Product product);
+        IEnumerable<Product> GetAll();
+    }
+}
